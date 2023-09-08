7b4140c53ffd67fd41fdc54c328c855f5a7088b2
Doina Colun <doinyco@gmail.com>
2023-09-07 22:55:01 -0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { Link } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>I am App</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
